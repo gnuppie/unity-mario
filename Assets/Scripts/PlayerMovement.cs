9adb614641da6224a7479bc44918e581b8b6f715
gnuppie <pung.junhao.louis@gmail.com>
2023-09-13 13:09:49 +0800

    Mario not turning direction, UI yet to implement fully

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10;
+    public float maxspeed = 20;
+
+    public float upSpeed = 10;
+    private bool onGroundState = true;
+    private Rigidbody2D marioBody;
+
+    // global variables
+    private SpriteRenderer marioSprite;
+    private bool faceRightState = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        marioSprite = GetComponent<SpriteRenderer>();
+        // Set to be 30 FPS
+        Application.targetFrameRate = 30;
+        marioBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // toggle state
+        if (Input.GetKeyDown("a") && faceRightState)
+        {
+            faceRightState = false;
+            marioSprite.flipX = true;
+        }
+
+        if (Input.GetKeyDown("d") && !faceRightState)
+        {
+            faceRightState = true;
+            marioSprite.flipX = false;
+        }
+    }
+
+    // FixedUpdate is called 50 times a second
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Mathf.Abs(moveHorizontal) > 0)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            // check if it doesn't go beyond maxSpeed
+            if (marioBody.velocity.magnitude < maxspeed)
+                marioBody.AddForce(movement * speed);
+        }
+
+        // stop
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+        {
+            // stop
+            marioBody.velocity = Vector2.zero;
+        }
+
+        // other instructions
+
+        if (Input.GetKeyDown("space") && onGroundState)
+        {
+            marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+            onGroundState = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground")) onGroundState = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Collided with goomba!");
+        }
+    }
+}
