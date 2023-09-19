acf93a236a86bdc719869d9d755f9fd3e035c892
gnuppie <pung.junhao.louis@gmail.com>
2023-09-20 00:14:59 +0800

    Physics2D Done, left Week 2 Checkoff

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -125,9 +125,11 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    int collisionLayerMask = (1 << 3) | (1 << 6) | (1 << 7);
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("Ground") && !onGroundState)
+
+        if (((collisionLayerMask & (1 << col.transform.gameObject.layer)) > 0) & !onGroundState)
         {
             onGroundState = true;
             // update animator state
